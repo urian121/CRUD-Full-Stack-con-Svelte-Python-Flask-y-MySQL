bde18cee4484c5b5a1f93502a6cdf3676c3123e3
URIAN VIERA <urian1213viera@gmail.com>
2025-02-03 17:29:42 -0500

    trabajando en el front

---
+++ frontend/my-contact-app/src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  // Importar Bootstrap CSS y sus iconos
+  import 'bootstrap/dist/css/bootstrap.min.css';
+  import 'bootstrap-icons/font/bootstrap-icons.css';
+  
+  import Formulario from './lib/Formulario.svelte';
+  import Contactos from './lib/Contactos.svelte';
+</script>
+
+<div class="container">
+  <h1>Vite + Svelte</h1>
+
+  <div class="row">
+    <div class="col-md-4">
+      <Formulario />
+    </div>
+
+    <div class="col-md-8">
+      <Contactos />
+    </div>
+  </div>
+</div>
